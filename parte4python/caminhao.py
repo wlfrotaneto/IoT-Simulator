8cc83e44c55c0c3c41836c6b3da004f036840ac7
Walter Neto <walter.lopes.frota.neto@gmail.com>
2021-04-19 02:53:59 -0300

    fix python

--- parte4python/caminhao.py
+++ parte4python/caminhao.py
@@ -1,12 +1,9 @@
 import socket, random, time
 
-nameClient = "Caminhao";
-caminhaoID = "666";
-centralID = "2424";
-containerID = "1691";
-
-HOST = '6.tcp.ngrok.io'
-PORT = 17115
+nameClient = "Caminhao"
+caminhaoID = "666"
+centralID = "2424"
+containerID = "1691"
 
 devicesTxt = open('lista_dispositivos.txt', 'r')
 linesDevices = devicesTxt.readlines()
@@ -31,33 +28,35 @@ def connectContainer(HOST, PORT):
         data = s.recv(1024)
     return repr(data)
 
-def runSimulation():
-    centralHost = findServerHost(centralID)
-    print(centralHost[1])
-    print(centralHost[2].split('\n')[0])
+def waitRandomTime(message):
+    randomTime = random.randint(5, 20)
+    print(message + ": " + str(randomTime))
+    time.sleep(randomTime)
 
+def runSimulation():
+    #Searches Central TCP Host
+    centralHost = findServerHost(centralID)
+    print('Central Host: ' + centralHost[1] + ' | ' + centralHost[2].split('\n')[0])
+
+    #Connects to Central TCP Server
     centralData = connectCentral(centralHost[1], int(centralHost[2].split('\n')[0]))
     centralResponse = centralData.split("b'")[1].split("'")[0]
     print('Central: ' + centralResponse)
-    # print(centralResponse.split('COLETAR ')[1])
+    
+    #Searches Container TCP Host
+    containerID = centralResponse.split('COLETAR ')[1]
+    containerHost = findServerHost(containerID)
+    print('Container Host: ' + containerHost[1] + ' | ' + containerHost[2].split('\n')[0])
 
-    randomTime = random.randint(5, 20)
-    print("Tempo para chegar ao Container: " + str(randomTime))
-    time.sleep(randomTime)
+    #Wait Time to arrive at Container
+    waitRandomTime('Tempo para chegar ao Container')
 
+    #Connects to Container TCP Server
+    containerData = connectContainer(containerHost[1], int(containerHost[2].split('\n')[0]))
+    containerResponse = containerData.split("b'")[1].split("'")[0]
+    print('Container: ' + containerResponse)
 
+    #Wait Time to arrive at Central
+    waitRandomTime('Tempo para retornar a Central')
 
-runSimulation()
-
-# lineHost = findServerHost('1691')
-# print(lineHost[2].split('\n')[0])
-
-# centralMessage = connectCentral(HOST, PORT)
-# print(centralMessage)
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     s.sendall(b'Hello, world')
-#     data = s.recv(1024)
-# 
-# print(repr(data))
+while(True): runSimulation()
